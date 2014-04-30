85f5c4ddf084e1c0c7f47bbe4eaaed4206eb64cb
ajoys <alvin.joys@gmail.com>
2014-04-30 11:49:02 -0400

    Files for program

---
+++ QuerySearch/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace QuerySearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.datepick = new System.Windows.Forms.MonthCalendar();
+            this.dropBox = new System.Windows.Forms.ComboBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Raw = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Tally = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pick a Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pick a Lane:";
+            // 
+            // datepick
+            // 
+            this.datepick.Location = new System.Drawing.Point(79, 19);
+            this.datepick.MaxSelectionCount = 1;
+            this.datepick.Name = "datepick";
+            this.datepick.ShowToday = false;
+            this.datepick.TabIndex = 2;
+            // 
+            // dropBox
+            // 
+            this.dropBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.dropBox.FormattingEnabled = true;
+            this.dropBox.Items.AddRange(new object[] {
+            "2230",
+            "0345",
+            "0630",
+            "1130",
+            "1430",
+            "1930",
+            "2130",
+            "All"});
+            this.dropBox.Location = new System.Drawing.Point(129, 272);
+            this.dropBox.Name = "dropBox";
+            this.dropBox.Size = new System.Drawing.Size(121, 21);
+            this.dropBox.TabIndex = 3;
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(129, 323);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(110, 23);
+            this.submit.TabIndex = 4;
+            this.submit.Text = "Submit Query";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(144, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // Raw
+            // 
+            this.Raw.AutoSize = true;
+            this.Raw.Checked = true;
+            this.Raw.Location = new System.Drawing.Point(6, 19);
+            this.Raw.Name = "Raw";
+            this.Raw.Size = new System.Drawing.Size(73, 17);
+            this.Raw.TabIndex = 6;
+            this.Raw.TabStop = true;
+            this.Raw.Text = "Raw Data";
+            this.Raw.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Tally);
+            this.groupBox1.Controls.Add(this.Raw);
+            this.groupBox1.Location = new System.Drawing.Point(73, 193);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 58);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pick a Form Type";
+            // 
+            // Tally
+            // 
+            this.Tally.AutoSize = true;
+            this.Tally.Location = new System.Drawing.Point(111, 19);
+            this.Tally.Name = "Tally";
+            this.Tally.Size = new System.Drawing.Size(73, 17);
+            this.Tally.TabIndex = 7;
+            this.Tally.TabStop = true;
+            this.Tally.Text = "Tally Data";
+            this.Tally.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 395);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.dropBox);
+            this.Controls.Add(this.datepick);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Search Lanes";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox dropBox;
+        private System.Windows.Forms.Button submit;
+        internal System.Windows.Forms.MonthCalendar datepick;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton Raw;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Tally;
+    }
+}
+
---
+++ QuerySearch/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace QuerySearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            //Getting the date picked from calender
+            string date = datepick.SelectionRange.Start.ToString().Substring(0,10);
+            //Lane
+            string lane = dropBox.Text.ToString();
+            //Rearanging the date to match the SQL database
+            string day = date.Substring(0, 2);
+            string month = date.Substring(3, 2);
+            string year = date.Substring(6, 4);
+            if (int.Parse(day) < 10)
+            {
+                day = day.Replace("0", "");
+            }
+            if (int.Parse(month) < 10)
+            {
+                month = month.Replace("0", "");
+            }
+            date = month + "/" + day + "/" + year;
+
+            //If the user wants to see all the parts scanned from that day. 
+            if (lane == "All")
+            {
+                //opening connection
+                string MyConnectionString = "Data Source=OSH4DPC005\\SQLEXPRESS;Initial Catalog=DMZ;Integrated Security=SSPI; User ID=user1; Password = dmzdatabase;";
+                SqlConnection connection = new SqlConnection(MyConnectionString);
+
+                //Adding a % sign to the date so the query will return any part that has this date in its lane number
+                date = date + "%";
+
+                try
+                {
+                    //opening conenction and setting up the query
+                    connection.Open();
+
+                    //RAW DATA
+                    if (Raw.Checked == true)
+                    {
+                        SqlCommand cmd = new SqlCommand("SELECT * FROM Parts WHERE Truck_Number LIKE @trucknumber", connection);
+                        cmd.Parameters.AddWithValue("@trucknumber", date);
+
+                        SqlDataReader dr;
+
+                        dr = cmd.ExecuteReader();
+                        int i = 0;
+                        //Creating name for the file
+                        string truckinfo = date.Replace("/", "-").Replace("%", "");
+
+                        //Writing all the data from the truck into a file
+                        date = date.Replace("%", "");
+                        using (StreamWriter sw = new StreamWriter("S:\\Share\\DMZ\\Reports\\"+truckinfo + "_fullday_raw.txt"))
+                        {
+                            //Header
+                            sw.WriteLine("   {0, 10}{1,10}{2,10}    {3,-23}{4,-10}", "Part Number", "Total", "Packs", "Serial Number", "Type");
+                            while (dr.Read())
+                            {
+                                //Creating Numbers along the side
+                                i++;
+                                if (i >= 10)
+                                {
+                                    sw.Write("{0,1}.", i);
+                                }
+                                else
+                                {
+                                    sw.Write("{0,1}. ", i);
+                                }
+
+                                // Counts to 4, each piece of data in the database
+                                for (int j = 0; j <= 4; j++)
+                                {
+                                    if (j == 3)
+                                    {
+                                        sw.Write("    {0, -23}", dr.GetString(4));
+                                    }
+                                    else if (j == 1 || j == 2)
+                                    {
+                                        sw.Write("{0, 10}", dr.GetInt32(j));
+                                    }
+                                    else if (j == 4)
+                                    {
+                                        sw.Write("{0,-10}", dr.GetString(6));
+                                    }
+                                    else
+                                    {
+                                        sw.Write("{0, 10}", dr.GetString(j));
+                                    }
+
+                                }
+                                sw.WriteLine();
+
+                            }
+                        }
+                        connection.Close();
+                        MessageBox.Show("Report Created!");
+                    }
+
+                    //TALLY DATA
+                    else if (Tally.Checked == true)
+                    {
+                        //Query
+                        SqlCommand cmd = new SqlCommand("SELECT Part_Number, SUM(Total), SUM(Number_of_Packs) FROM Parts WHERE Truck_Number LIKE @TruckNumber GROUP BY Part_Number", connection);
+                        cmd.Parameters.AddWithValue("@TruckNumber", date);
+
+                        SqlDataReader dr;
+
+                        dr = cmd.ExecuteReader();
+                        int i = 0;
+                        //Creating FIle Name
+                        string truckinfo = date.Replace("/", "-").Replace("%", "");
+
+                        //Writing all the data from the truck into a file
+                        date = date.Replace("%", "");
+                        using (StreamWriter sw = new StreamWriter("S:\\Share\\DMZ\\Reports\\" + truckinfo + "_fullday_tally.txt"))
+                        {
+                            sw.WriteLine("   {0, 10}{1,10}{2,10}", "Part Number", "Total", "Packs");
+                            sw.WriteLine("---------------------------------");
+                            sw.WriteLine();
+                            while (dr.Read())
+                            {
+                                i++;
+                                if (i >= 10)
+                                {
+                                    sw.Write("{0,1}.", i);
+                                }
+                                else
+                                {
+                                    sw.Write("{0,1}. ", i);
+                                }
+
+                                // Counts to 2, 0 for Part Number, 1 for total, 2 for number of packs
+                                for (int j = 0; j <= 2; j++)
+                                {
+                                    if (j == 1 || j == 2)
+                                    {
+                                        sw.Write("{0, 10}", dr.GetInt32(j));
+                                    }
+                                    else
+                                    {
+                                        sw.Write("{0, 10}", dr.GetString(j));
+                                    }
+
+                                }
+                                sw.WriteLine();
+                                sw.WriteLine("---------------------------------");
+
+                            }
+                        }
+                        connection.Close();
+                        MessageBox.Show("Report Created!");
+                    }
+                  
+                }
+                catch (Exception p)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+            else if (lane == "") 
+            {
+                MessageBox.Show("Please Select a Lane");
+            }
+            else 
+            {
+                //opening connection - This is where the info needed to open the database 
+                string MyConnectionString = "Data Source=OSH4DPC005\\SQLEXPRESS;Initial Catalog=DMZ;Integrated Security=SSPI; User ID=user1; Password = dmzdatabase;";
+                SqlConnection connection = new SqlConnection(MyConnectionString);
+
+                try
+                {
+                    string truckNumber = date + " -" + lane;
+                    //opening conenction and setting up the query
+                    connection.Open();
+
+                    //RAW DATA
+                    if (Raw.Checked == true)
+                    {
+                        SqlCommand cmd = new SqlCommand("SELECT * FROM Parts WHERE Truck_Number LIKE @trucknumber ORDER BY Part_Number", connection);
+                        cmd.Parameters.AddWithValue("@trucknumber", truckNumber);
+                        SqlCommand op = new SqlCommand("SELECT Operator FROM Parts WHERE Truck_Number LIKE @TruckNumber", connection);
+                        op.Parameters.AddWithValue("@trucknumber", truckNumber);
+
+                        SqlDataReader dr;
+                        SqlDataReader newdr;
+
+                        newdr = op.ExecuteReader();
+                        newdr.Read();
+                        string name = newdr.GetString(0);
+                        newdr.Close();
+                        dr = cmd.ExecuteReader();
+
+                        int i = 0;
+                        string truckinfo = truckNumber.Replace("/", "-").Replace("%", "");
+
+                        //Writing all the data from the truck into a file
+                        using (StreamWriter sw = new StreamWriter("S:\\Share\\DMZ\\Reports\\" + truckinfo + "_raw.txt"))
+                        {
+                            sw.WriteLine("   {0, 10}{1,10}{2,10}    {3,-23}{4,-10}", "Part Number", "Total", "Packs", "Serial Number", "Type");
+                            sw.WriteLine("   Operator: {0}", name);
+                            while (dr.Read())
+                            {
+                                i++;
+                                if (i >= 10)
+                                {
+                                    sw.Write("{0,1}.", i);
+                                }
+                                else
+                                {
+                                    sw.Write("{0,1}. ", i);
+                                }
+
+                                // Counts to 4, each piece of data in the database
+                                for (int j = 0; j <= 4; j++)
+                                {
+                                    if (j == 3)
+                                    {
+                                        sw.Write("    {0, -23}", dr.GetString(4));
+                                    }
+                                    else if (j == 1 || j == 2)
+                                    {
+                                        sw.Write("{0, 10}", dr.GetInt32(j));
+                                    }
+                                    else if (j == 4)
+                                    {
+                                        sw.Write("    {0,-10}", dr.GetString(6));
+                                    }
+                                    else
+                                    {
+                                        sw.Write("{0, 10}", dr.GetString(j));
+                                    }
+
+                                }
+                                sw.WriteLine();
+
+                            }
+                        }
+                        connection.Close();
+                        MessageBox.Show("Report Created!");
+                    }
+
+                    //TALLY DATA
+                    else if (Tally.Checked == true)
+                    {
+                        //Query
+                        SqlCommand cmd = new SqlCommand("SELECT Part_Number, SUM(Total), SUM(Number_of_Packs) FROM Parts WHERE Truck_Number LIKE @TruckNumber GROUP BY Part_Number", connection);
+                        cmd.Parameters.AddWithValue("@TruckNumber", truckNumber);
+                        SqlCommand op = new SqlCommand("SELECT Operator FROM Parts WHERE Truck_Number LIKE @TruckNumber", connection);
+                        op.Parameters.AddWithValue("@trucknumber", truckNumber);
+
+                        SqlDataReader dr;
+                        SqlDataReader newdr;
+
+                        newdr = op.ExecuteReader();
+                        newdr.Read();
+                        string name = newdr.GetString(0);
+                        newdr.Close();
+
+                        dr = cmd.ExecuteReader();
+                        int i = 0;
+                        //Creating FIle Name
+                        string truckinfo = truckNumber.Replace("/", "-").Replace("%", "");
+
+                        //Writing all the data from the truck into a file
+                        date = date.Replace("%", "");
+                        using (StreamWriter sw = new StreamWriter("S:\\Share\\DMZ\\Reports\\" + truckinfo + "_tally.txt"))
+                        {
+                            sw.WriteLine("   {0, 10}{1,10}{2,10}          Operator:{3}", "Part Number", "Total", "Packs", name);
+                            sw.WriteLine("---------------------------------");
+                            sw.WriteLine();
+                            while (dr.Read())
+                            {
+                                i++;
+                                if (i >= 10)
+                                {
+                                    sw.Write("{0,1}.", i);
+                                }
+                                else
+                                {
+                                    sw.Write("{0,1}. ", i);
+                                }
+
+                                // Counts to 2, 0 for Part Number, 1 for total, 2 for number of packs
+                                for (int j = 0; j <= 2; j++)
+                                {
+                                    if (j == 1 || j == 2)
+                                    {
+                                        sw.Write("{0, 10}", dr.GetInt32(j));
+                                    }
+                                    else
+                                    {
+                                        sw.Write("{0, 10}", dr.GetString(j));
+                                    }
+
+                                }
+                                sw.WriteLine();
+                                sw.WriteLine("---------------------------------");
+
+                            }
+                        }
+                        connection.Close();
+                        MessageBox.Show("Report Created!");
+                    }
+                    
+                }
+                
+                catch (Exception p)
+                {
+                    MessageBox.Show(p.ToString());
+                }
+           } 
+        }   
+
+    }
+}
